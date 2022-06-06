85e42145eaaf2a5fd60d4483257bf1037ca397f2
Taeho-Kim <taehokim@hicare.net>
2022-06-06 12:25:29 +0900

    Initial Commit

---
+++ TodoExample/TodoExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoExampleApp.swift
+//  TodoExample
+//
+//  Created by 김태호 on 2022/06/06.
+//
+
+import SwiftUI
+
+@main
+struct TodoExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
